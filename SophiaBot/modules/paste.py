c14fe826295dfe11c87b7446b869dd8bf2ea32fa
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-03 20:59:49 +0530

    Update paste.py

--- SophiaBot/modules/paste.py
+++ SophiaBot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SophiaBot import dispatcher
-from SophiaBot.modules.disable import DisableAbleCommandHandler
+from Rosi import dispatcher
+from Rosi.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
