70daefd0f3a339cfd3eef6b18061b0cfe9cc45a5
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-03 18:49:04 +0530

    Update bans.py

--- SophiaBot/modules/bans.py
+++ SophiaBot/modules/bans.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from SophiaBot import (
+from Rosi import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -15,8 +15,8 @@ from SophiaBot import (
     WOLVES,
     dispatcher,
 )
-from SophiaBot.modules.disable import DisableAbleCommandHandler
-from SophiaBot.modules.helper_funcs.chat_status import (
+from Rosi.modules.disable import DisableAbleCommandHandler
+from Rosi.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
@@ -27,9 +27,9 @@ from SophiaBot.modules.helper_funcs.chat_status import (
     user_can_ban,
     can_delete,
 )
-from SophiaBot.modules.helper_funcs.extraction import extract_user_and_text
-from SophiaBot.modules.helper_funcs.string_handling import extract_time
-from SophiaBot.modules.log_channel import gloggable, loggable
+from Rosi.modules.helper_funcs.extraction import extract_user_and_text
+from Rosi.modules.helper_funcs.string_handling import extract_time
+from Rosi.modules.log_channel import gloggable, loggable
 
 
 @run_async
