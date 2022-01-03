fe4a624b27c91b6437e93fc33165fd79d3919739
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-03 21:26:16 +0530

    Update sed.py

--- SophiaBot/modules/sed.py
+++ SophiaBot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from SophiaBot import LOGGER, dispatcher
-from SophiaBot.modules.disable import DisableAbleMessageHandler
-from SophiaBot.modules.helper_funcs.regex_helper import infinite_loop_check
+from Rosi import LOGGER, dispatcher
+from Rosi.modules.disable import DisableAbleMessageHandler
+from Rosi.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
